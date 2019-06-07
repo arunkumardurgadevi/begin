d82b4c1dd96f4f6faa8f0f917df7743147871696
arunkumardurgadevi <durgaarun.nm@gmail.com>
2019-06-07 16:23:44 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,8 +1,8 @@
 import math
 import functools
 n1,k=map(int,input().split())
-l=[int(i) for i in input().split()]
+p=[int(i) for i in input().split()]
 for i in range(k):
     c,d=map(int,input().split())
-    t=functools.reduce(math.gcd,l[c-1:d])
+    t=functools.reduce(math.gcd,p[c-1:d])
     print(t)
